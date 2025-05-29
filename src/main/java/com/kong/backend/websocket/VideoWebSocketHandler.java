b09fa9dc8f54a17e765cc84c25b0adc4d3b87a92
HanHeumJo <cooper13210@gmail.com>
2025-05-29 09:26:42 +0900

    websoket 리빌딩
    websoket 리빌딩

---
+++ src/main/java/com/kong/backend/websocket/VideoWebSocketHandler.java
@@ -0,0 +1,30 @@
+package com.kong.backend.websocket;
+
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class VideoWebSocketHandler extends TextWebSocketHandler {
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen() && !s.getId().equals(session.getId())) {
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
