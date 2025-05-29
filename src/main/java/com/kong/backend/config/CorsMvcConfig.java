c1aeaa1fa79f7892824ca8fc9a182e6fb0f9d465
JHH <cooper13210@gmail.com>
2025-05-29 14:22:56 +0900

    CORS 추가
    CORS 추가

---
+++ src/main/java/com/kong/backend/config/CorsMvcConfig.java
@@ -0,0 +1,21 @@
+package com.kong.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//CORS 설정 (MVC)
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://kongback.kro.kr:8080"
+                        ,"https://localhost:8080", "http://localhost")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
