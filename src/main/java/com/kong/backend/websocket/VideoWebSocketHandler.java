c65ff194db9c7dbb86da49562e0f280e97bbc5aa
HanHeumJo <cooper13210@gmail.com>
2025-06-15 00:10:35 +0900

    웹소켓 욜로주소 수정
    웹소켓 욜로주소 수정

--- src/main/java/com/kong/backend/websocket/VideoWebSocketHandler.java
+++ src/main/java/com/kong/backend/websocket/VideoWebSocketHandler.java
@@ -77,7 +77,7 @@ public class VideoWebSocketHandler extends TextWebSocketHandler {
                         }
                     }
                 }
-            }, "ws://106.101.8.112/ws/fall");
+            }, "ws://116.43.155.170/ws/fall");
 
         } catch (Exception e) {
             System.out.println("❌ YOLO 서버 연결 실패: " + e.getMessage());
